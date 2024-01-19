fa98e185c6267b521a658e199c9f99a43f51099f
ADMIN <ADMIN@PC328>
2024-01-19 11:23:02 +0700

    Add project files.

---
+++ BT_Banking01/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_Banking01
+{
+    class Account
+    {
+        private double _balace;
+        public Account() { }
+        public Account(double balance)
+        {
+            this._balace = balance;
+        }
+
+        protected double Balance
+        {
+            set { _balace = value; }
+            get { return _balace; }
+        }
+        public virtual bool WithDraw(double amount)
+        {
+            return false;
+        }
+        public virtual bool Deposit(double amount)
+        {
+            return false;
+        }
+        public void PrintBalance()
+        {
+            Console.WriteLine("Balance: {0}", _balace);
+        }
+        class SavigAccount : Account
+        {
+            private double _interesRate = 0.8;
+
+            public SavigAccount() : base() { }
+            public SavigAccount(double balance) : base(balance)
+            {
+            }
+            public double InteresRate
+            {
+                set { _interesRate = value; }
+                get { return _interesRate; }
+            }
+            public override bool WithDraw(double amount)
+            {
+                return base.WithDraw(amount);
+            }
+            public override bool Deposit(double amount)
+            {
+                return base.Deposit(amount);
+            }
+        }
+        class CheckingAccount : Account
+        {
+            public CheckingAccount() : base() { }
+            public CheckingAccount(double balance) : base(balance)
+            {
+            }
+            public override bool WithDraw(double amount)
+            {
+                return false;
+            }
+            public override bool Deposit(double amount)
+            {
+                return false;
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Account ac1 = new SavigAccount(200);
+                Console.WriteLine("Saving Account");
+                ac1.Deposit(100);
+                ac1.PrintBalance();
+
+                Account ac2 = new SavigAccount(200);
+                Console.WriteLine("Saving Account");
+                ac2.Deposit(100);
+                ac2.PrintBalance();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Lienketdanhsachdon/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lienketdanhsachdon
+{
+    class Node
+    {
+        private int info;
+        private Node next;
+        public Node(int x)
+        {
+            info = x;
+            next = null;
+        }
+        public int Info
+        {
+            set { this.info = value; }
+            get { return info; }
+        }
+        public Node Next
+        {
+            set { this.next = value; }
+            get { return next; }
+        }
+    }
+    class Singlelinklist
+    {
+        private Node Head;
+        public Singlelinklist()
+        {
+            Head = null;
+        }
+        public void Adhead(int x)
+        {
+            Node p = new Node(x);
+            p.Next = Head;
+            Head = p;
+        }
+        public void Addlast(int x)
+        {
+            Node p = new Node(x);
+            if (Head == null)
+            {
+                Head = p;
+            }
+            else
+            {
+                Node q = Head;
+                while(p.Next != null)
+                {
+                    q = q.Next;
+                }
+                p.Next = q;
+            }
+        }
+        public void Processlist()
+        {
+            Node p = Head;
+            while (p != null)
+            {
+                Console.Write($"{p.Info} ");
+                p = p.Next;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singlelinklist ds = new Singlelinklist();
+            ds.Adhead(9);
+            ds.Adhead(5);
+            ds.Adhead(6); 
+            ds.Adhead(7);
+            Console.WriteLine("Danh sach lien ket: ");
+            ds.Processlist();
+            Console.ReadLine();
+        }
+    }
+}
